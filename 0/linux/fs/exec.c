d208991b79c82bf407b8faa82a18a856f3c19381
Robot-xiaoliu <liuzheng63@outlook.com>
2022-06-23 11:34:46 +0800

    删去不需要的代码，优化sleep函数

--- 0/linux/fs/exec.c
+++ 0/linux/fs/exec.c
@@ -21,6 +21,7 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <a.out.h>
+#include <unistd.h>
 
 #include <linux/fs.h>
 #include <linux/sched.h>
@@ -44,23 +45,62 @@ int sys_uselib()
 }
 // 修改 
 // int sys_execve2(const char *path, char ** argv, char **envp){
-// 	// unsigned long eip[4] = {1,1,0,0};
+//     /*
+//     功能：
+//         以⽴即加载⽅式执⾏⼀个指定的程序。此系统调⽤开始后，该进程不应再发⽣代码段和数据段中的缺⻚故障。
+//     输入：
+//         path: 待执⾏程序路径名称，
+//         argv: 程序的参数，
+//         envp: 环境变量的数组指针
+//     返回值：
+//         成功不返回，失败返回-1
+//     */
+// 	unsigned long eip[4] = {1,1,0,0};
 // 	do_execve(eip,1,path,argv,envp);
 // 	return -1;
 // }
 int  sys_getdents(unsigned int fd, struct linux_dirent *dirp, unsigned int count){
+	/*
+    功能：
+        获取⽬录的⽬录项。
+    输⼊：
+        fd：所要读取⽬录的⽂件描述符。
+        dirp：⼀个缓存区，⽤于保存所读取⽬录的信息。缓存区的结构如下：
+    返回值：
+        成功执⾏，返回读取的字节数。当到⽬录结尾，则返回0。失败，则返回-1
+    */
 	printk("sys now ok");
 	return 0;
 }
 unsigned int sys_sleep(unsigned int seconds){
-	int k =0 ;
-	while(k<=seconds*1000000)//{
-		k++;
-	sys_alarm(seconds);	
+	/*
+    功能：
+        执⾏进程睡眠；
+    输⼊：
+        睡眠的时间间隔；seconds: 秒
+    返回值：
+        成功返回0，失败返回-1;
+    */
+	int i=0;
+	(void)sys_signal(SIGALRM,SIG_IGN);
+	for(;i<seconds;++i){
+		sys_alarm(1);
+		pause();
+	}
 	return 0;
 }
 char *sys_getcwd(char * buf, size_t size){
+	/*
+    功能：
+        获取当前⼯作⽬录； 
+    输⼊：
+        char *buf：⼀块缓存区，⽤于保存当前⼯作⽬录的字符串。当buf设为NULL，由系统来分配缓存区。
+        size：buf缓存区的⼤⼩。
+    返回值：
+        成功执⾏，则返回当前⼯作⽬录的字符串的指针。失败，则返回NULL。
+    */
 	buf = (char *)malloc(size);
+	// 完成getcwd:
 	printk("ok in sys getcwd,%d\n",size);
 	return buf;
 }
